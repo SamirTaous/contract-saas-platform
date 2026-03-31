0de683b83ea394bdc47323a664ee4673b9a6d01d
SamirTaous <hgta626@gmail.com>
2026-03-31 15:54:09 +0100

    Initial project structure with auth service

---
+++ auth-service/src/test/java/com/samir/auth/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samir.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
