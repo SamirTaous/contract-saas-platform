7dd54caee7aea217d0ba980c1702424a8eb98ee2
SamirTaous <hgta626@gmail.com>
2026-05-06 16:15:15 +0100

    refactor(budget): add all args constructor for tests

--- operation-service/src/main/java/com/samir/ops/dto/BudgetFilterDTO.java
+++ operation-service/src/main/java/com/samir/ops/dto/BudgetFilterDTO.java
@@ -1,11 +1,13 @@
 package com.samir.ops.dto;
 
 import com.samir.ops.model.Type;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 @Data
 @RequiredArgsConstructor
+@AllArgsConstructor
 public class BudgetFilterDTO {
     Type type;
     String article;
